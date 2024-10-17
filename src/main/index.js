29fc6e7ddedbd82cdf002cfbeb588e4316c84418
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-10-17 14:26:15 +0000

    Experimental.

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.info(`Welcome to Lightingale Hyacinth!`);
+let emptyBuffer = new Uint8Array(0);
+
+let handleRequest = async function (request) {
+	console.debug(request.url);
+	return new Response(emptyBuffer, {
+		status: 302,
+		headers: {
+			"Location": "https://ltgc.cc/"
+		}
+	})
+};
+
+addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event.request));
+});
